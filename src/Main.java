584ef62a07dc27b0cdbae6f344f8860cc6769ed3
Maksat <aylbekov9@gmail.com>
2026-09-19 11:00:00 +0500

    feat: update main.java client side code, and line break

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import abstractfactory.RegionalTerminalClient;
+import abstractfactory.PaymentRegionalFactory;
+import abstractfactory.UsaPaymentFactory;
+import abstractfactory.EuPaymentFactory;
 import factorymethod.TerminalCreator;
 import factorymethod.VisaTerminalCreator;
 
@@ -5,4 +9,9 @@ void main() {
     IO.println("<<< PART A: Factory Method >>>");
     TerminalCreator terminal = new VisaTerminalCreator();
     terminal.executePayment(150.0);
+
+    IO.println("\n<<< PART B: Abstract Factory >>>");
+    PaymentRegionalFactory factory = new UsaPaymentFactory();
+    RegionalTerminalClient client = new RegionalTerminalClient(factory);
+    client.processOrder(150.0);
 }
--- src/abstractfactory/UsaValidator.java
+++ src/abstractfactory/UsaValidator.java
@@ -3,7 +3,7 @@ package abstractfactory;
 public class UsaValidator implements Validator{
     @Override
     public boolean validate(double amount) {
-        IO.print("Validating transaction via US banking security standard...");
+        IO.print("Validating transaction via US banking security standard...\n");
         return amount > 0 && amount < 10000;
     }
 }
